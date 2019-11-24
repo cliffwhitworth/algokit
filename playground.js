324d48227a590356f4d2c18b877122d7d37f00a8
Cliff Whitworth <cliff.whitworth@gmail.com>
2019-11-24 14:05:41 -0600

    clean up

--- playground.js
+++
@@ -1,9 +0,0 @@
-const anagram = s => {
-    return s.toLowerCase().match(/\S/g).sort().join('');
-}
-
-a1 = 'Eleven plus two';
-a2 = 'Twelve plus one';
-console.log(anagram(a1));
-console.log(anagram(a2));
-console.log(anagram(a1) === anagram(a2));
--- playground.py
+++
@@ -1,9 +0,0 @@
-import re
-def anagram(s: str) -> str:
-    return ''.join(sorted(re.findall('\\S', s.lower())))
-
-a1 = 'Eleven plus two'
-a2 = 'Twelve plus one'
-print(anagram(a1))
-print(anagram(a2))
-print(anagram(a1) == anagram(a2))
